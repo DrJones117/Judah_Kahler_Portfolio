333c89a9de4d46549cbc322d311157d3e08de879
Judah Kahler <judah.kahler@gmail.com>
2024-05-31 20:03:31 -0500

    Get profile set up

--- obj/Debug/net8.0/Judah_Kahler_Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Judah_Kahler_Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Judah_Kahler_Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6583222d773e5f4fcada90b9a81e5697ec0cf575")]
 [assembly: System.Reflection.AssemblyProductAttribute("Judah_Kahler_Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Judah_Kahler_Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
